c2c757c47618ee47d127873b07bde0b15f408014
Aji Sahputra <aji.saputro@studens.amikom.ac.id>
2020-10-15 01:15:35 +0700

    IntentApp

---
+++ app/src/main/java/com/example/myintentapp/DialNumber.java
@@ -0,0 +1,14 @@
+package com.example.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DialNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dial_number);
+    }
+}
